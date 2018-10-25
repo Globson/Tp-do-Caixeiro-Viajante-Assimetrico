3d0b52f565c98b6b7eaadd249a572ae8b3be3eac
Numb4r <yuri.d.d.faria@gmail.com>
2018-10-25 03:58:48 +0000

    Criacao dos arquivos de estilizacao do menu

---
+++ Libs/Menu.h
@@ -0,0 +1,3 @@
+void cabecalho();
+void rodape();
+void interativo();
---
+++ Sources/Menu.c
@@ -0,0 +1,13 @@
+#include "../Libs/Menu.h"
+#include <stdio.h>
+void cabecalho(/* arguments */) {
+  /* code */
+}
+void rodape(/* arguments */) {
+  /* code */
+}
+void interativo(/* arguments */) {
+  printf("Deseja entrar com arquivo ou interativo?\n" );
+  printf("1.Arquivo\n");
+  printf("2.Interativo\n");
+}
