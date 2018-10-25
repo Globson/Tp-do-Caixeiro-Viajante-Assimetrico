8792b9cbd11832d143973c77eaec4501fe034100
Globson <samuelpedro1999@gmail.com>
2018-10-25 00:21:11 -0300

    Adaptando

--- main.c
+++ main.c
@@ -2,10 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 int main(int argc, char const *argv[]) {
-  int a=1,b=2,*c,d;
-  c=&a;
-  d=*c+b+b;
-  *c=d;
-printf("Os valores sao: %d %d %d %d",a,b,*c,d);
+
   return 0;
 }
