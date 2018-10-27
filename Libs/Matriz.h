4c3b7095af106c2dd456e5e9c4da345b9bac96e6
Numb4r <yuri.d.d.faria@gmail.com>
2018-10-27 17:04:51 +0000

    Criado dois arquivos para modularizacao

---
+++ Libs/Matriz.h
@@ -0,0 +1,7 @@
+#ifndef  MATRIZ_H
+#define  MATRIZ_H
+void PreencheCustos(int QuantidadeCidades,int Matriz[QuantidadeCidades][QuantidadeCidades]);
+void PrintCustos(int QuantidadeCidades,int Matriz[QuantidadeCidades][QuantidadeCidades]);
+void PreencherParaPermutacao(int QuantidadeCidades,int vetor[QuantidadeCidades-1],int CidadeInicial);
+
+#endif /* end of include guard: MATRIZ_H */
---
+++ Sources/Matriz.c
@@ -0,0 +1,28 @@
+#include "../Libs/Matriz.h"
+#include <stdio.h>
+#include <stdlib.h>
+void PreencheCustos(int QuantidadeCidades,int Matriz[QuantidadeCidades][QuantidadeCidades]){
+  for ( size_t i = 0; i < QuantidadeCidades; i++)
+    for ( size_t j = 0; j < QuantidadeCidades; j++)
+      Matriz[i][j]=(i == j ? 0 : 1+(rand() % 100));
+}
+void PrintCustos(int QuantidadeCidades,int Matriz[QuantidadeCidades][QuantidadeCidades]){
+  printf("MatrizCusto %d por %d\n",QuantidadeCidades,QuantidadeCidades );
+  for (size_t i = 0; i < QuantidadeCidades; i++) {
+    for (size_t j = 0; j < QuantidadeCidades; j++)
+      printf("  %d",Matriz[i][j] );
+    printf("\n");
+  }
+}
+void PreencherParaPermutacao(int QuantidadeCidades,int vetor[QuantidadeCidades-1],int CidadeInicial){
+  int aux = 0;
+  for (size_t i = 0; i < QuantidadeCidades-1; i++) {
+    if (i==CidadeInicial) {
+      aux++;
+      vetor[i]=aux;
+    }else{
+      vetor[i]=aux;
+    }
+    aux++;
+  }
+}
