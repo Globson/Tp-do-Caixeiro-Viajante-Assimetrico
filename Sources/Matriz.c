e0df6223156eb3abff68118e03a30a16168e51d9
Numb4r <yuri.d.d.faria@gmail.com>
2018-10-27 21:11:24 +0000

    Arrumado Erro Grafico

--- Sources/Matriz.c
+++ Sources/Matriz.c
@@ -7,7 +7,7 @@ void PreencheCustos(int QuantidadeCidades,int Matriz[QuantidadeCidades][Quantida
       Matriz[i][j]=(i == j ? 0 : 1+(rand() % 100));
 }
 void PrintCustos(int QuantidadeCidades,int Matriz[QuantidadeCidades][QuantidadeCidades]){
-  printf("MatrizCusto %d por %d\n",QuantidadeCidades,QuantidadeCidades );
+  printf("Matriz de Custo %d por %d\n",QuantidadeCidades,QuantidadeCidades );
   for (size_t i = 0; i < QuantidadeCidades; i++) {
     for (size_t j = 0; j < QuantidadeCidades; j++)
       printf("  %d",Matriz[i][j] );
